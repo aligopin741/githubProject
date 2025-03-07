878dacdce245ee1164d6b66f1231bc08d226f1bb
aligopin741 <aligopin741@gmail.com>
2025-03-07 14:26:20 +0000

    Add 2.js

---
+++ src/2.js
@@ -0,0 +1,11 @@
+// Create a new file called "index.js" in your project's root directory
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
